fd438e2f9f83eea0aa621ca88538c5c9088ff4a8
Valentin Rey <valen.9298@gmail.com>
2023-01-17 16:14:50 -0300

    fix fetch pricing

--- src/services/GetSearches.jsx
+++ src/services/GetSearches.jsx
@@ -3,13 +3,15 @@ export function GetSearches(word) {
     return fetch(`${API_URL}/sites/MLA/search?q=${word}`)
         .then(res => res.json())
         .then(data => {
+            console.log(data);
             const { results } = data
+            console.log(results);
             const InfoProductosAMostrar = results.map(prod => {
                 const title = prod.title
                 const id = prod.id
                 let thumbnail = prod.thumbnail
                 thumbnail = thumbnail.replace('-I.', '-O.');
-                const price = prod.prices.prices[0].amount
+                const price = prod.price
                 return { title, id, thumbnail, price }
             })
             return InfoProductosAMostrar
